3796ebdd679d5601400dd4a43d3c5d769ef2ef10
tomoemagica <57165129+tomoemagica@users.noreply.github.com>
2020-03-26 13:37:15 +0900

    Add files via upload

---
+++ m) data_dst Auto White Balance.bat
@@ -0,0 +1,23 @@
+@echo off
+call _internal\setenv.bat
+
+mkdir "%WORKSPACE%\data_dst" 2>nul
+
+"%PYTHON_EXECUTABLE%" "%DFL_ROOT%\main.py" videoed extract-video ^
+    --input-file "%WORKSPACE%\data_dst.*" ^
+    --output-dir "%WORKSPACE%\data_dst" ^
+    --fps 0
+
+py "%INTERNAL%\faceutil\awb.py"
+
+ffprobe -i %WORKSPACE%\data_dst.mp4 2>&1 | grep bitrate | gawk -F: '{print $6}' | gawk -F' ' '{print $1}' > tmp.txt
+
+"%PYTHON_EXECUTABLE%" "%DFL_ROOT%\main.py" videoed video-from-sequence ^
+    --input-dir "%WORKSPACE%\data_dst\awb" ^
+    --output-file "%WORKSPACE%\awb.mp4" ^
+    --reference-file "%WORKSPACE%\data_dst.*" ^
+    --include-audio < tmp.txt
+
+del tmp.txt
+
+pause
---
+++ m) data_src close eye.bat
@@ -0,0 +1,6 @@
+@echo off
+call _internal\setenv.bat
+
+py "%INTERNAL%\faceutil\close_eye.py"
+
+pause
